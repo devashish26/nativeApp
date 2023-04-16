2aa5c301268972922227e2a7e56dffd151ad7ddf
devashish <devashishkukade261@gmail.com>
2023-04-16 17:42:43 +0530

    Past history page added.

--- app/pages/PastHistory.js
+++ app/pages/PastHistory.js
@@ -1,30 +1,74 @@
 import { StyleSheet, TextInput, Button, View, Alert } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 
 
 const PastHistory = ({navigation}) => {
-  return (
-    <View style={styles.container}>
-        <TextInput placeholder='Address' inputMode='text'/>
-        <TextInput placeholder='Age' inputMode='numeric'/>
-        <TextInput placeholder='Allergies (Comma separated)' inputMode='text'/>
-        <TextInput placeholder='BP(mm/hg)' inputMode='numeric'/>
-        <TextInput placeholder='Blood Group' inputMode='text'/>
-        <TextInput placeholder='Condition' inputMode='text'/>
-        <TextInput placeholder='Current Medications' inputMode='text'/>
-        <TextInput placeholder='Disease' inputMode='text'/>
-        <TextInput placeholder='Gender' inputMode='text'/>
-        <TextInput placeholder='Height' inputMode='numeric'/>
-        <TextInput placeholder='Name' inputMode='text'/>
-        <TextInput placeholder='Symptoms (Comma separated)' inputMode='text'/>
-        <TextInput placeholder='Weight' inputMode='numeric'/>
-        <TextInput placeholder='Device ID' inputMode='text'/>
-        <View style={styles.container2}>
-            <Button title='Save' onPress={()=>{processInputs()}}/>
-            <Button title='Skip' onPress={()=>{Alert.alert('Please make sure to add details later.');navigation.navigate('test')}}/>
+
+    const [details, setDetails] = useState({
+        Address: "",
+        Age:"",
+        Allergies:{},
+        BP:"",
+        BloodGroup:"",
+        Condition:"",
+        Curr_Med:{},
+        Disease:"",
+        Gender:"",
+        Height:"",
+        Name:"",
+        Past_Med:{},
+        Pulse:"",
+        Symptoms:{},
+        Weight:"",
+        id:""
+    })
+    
+    function processInputs(){
+//e.nativeEvent.text
+    }
+    const handleChange =async (e)=>{
+        const value = e.nativeEvent.text;
+        const name = e._dispatchInstances.pendingProps.name;
+        console.log(name, value)
+        await setDetails(setDetails((prev)=>{
+            return {...prev, [name]: value}
+        }))
+        // await setDetails({...details,[name]:value})
+        console.log('details: ', details)
+        console.log('value: ',value)
+        console.log('name: ',name)
+        // e.nativeEvent.text
+        // e._dispatchInstances.pendingProps.name
+        
+        
+    }
+    function handleSubmit(){
+        console.log(details)
+    }
+    return (
+        <View style={styles.container}>
+            <TextInput onChange={handleChange} name='Address' placeholder='Address' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Age' placeholder='Age' inputMode='numeric'/>
+            <TextInput onChange={handleChange} name='Allergies' placeholder='Allergies (Comma separated)' inputMode='text'/>
+            <TextInput onChange={handleChange} name='BP' placeholder='BP(mm/hg)' inputMode='numeric'/>
+            <TextInput onChange={handleChange} name='BloodGroup' placeholder='Blood Group' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Condition' placeholder='Condition' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Curr_Med' placeholder='Current Medications' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Disease' placeholder='Disease' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Gender' placeholder='Gender' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Height' placeholder='Height' inputMode='numeric'/>
+            <TextInput onChange={handleChange} name='Name' placeholder='Name' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Past_Med' placeholder='Past Medications (Comma separated)' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Pulse' placeholder='Pulse' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Symptoms' placeholder='Symptoms (Comma separated)' inputMode='text'/>
+            <TextInput onChange={handleChange} name='Weight' placeholder='Weight' inputMode='numeric'/>
+            <TextInput onChange={handleChange} name='id' placeholder='Device ID' inputMode='text'/>
+            <View style={styles.container2}>
+                <Button title='Save' onPress={()=>{handleSubmit()}}/>
+                <Button title='Skip' onPress={()=>{Alert.alert('Please make sure to add details later.');navigation.navigate('test')}}/>
+            </View>
         </View>
-    </View>
-  )
+    )
 }
 
 const styles = StyleSheet.create({
