14a24585d21a3e52ca3e3cd9b4e21702f3fdf190
devashish <devashishkukade261@gmail.com>
2023-04-26 16:39:25 +0530

    Realtime data fetching done

--- app/compontents/RealtimeFile.js
+++ app/compontents/RealtimeFile.js
@@ -1,12 +1,37 @@
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {ref, onValue} from 'firebase/database'
+import { rdb } from '../firebase';
 
 const RealtimeFile = () => {
-  return (
-    <View style={styles.container}>
-      <Text>RealtimeFile</Text>
-    </View>
-  )
+    const [rtData, setRtData] = useState([]);
+
+    useEffect(()=>{
+        const myRef = ref(rdb, 'users/')
+        onValue(myRef, (snapshot)=>{
+            const data = snapshot.val();
+            const allPosts = Object.keys(data).map(key=>({
+                id:key, 
+                ...data[key]
+            }))
+            console.log(allPosts)
+            setRtData(allPosts)
+        })
+    },[])
+    return (
+        <View style={styles.container}>
+            {
+                rtData.map((item, index)=>{
+                    return(
+                        <View key={index}>
+                            <Text>{item.bp}</Text>
+                            <Text>{item.pulse}</Text>
+                        </View>
+                    )
+                })
+            }
+        </View>
+    )
 }
 
 export default RealtimeFile
